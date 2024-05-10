4dc12130c38254ef42f2abe3db066b16f106b745
Jordy Devrix <2109jordy2241@gmail.com>
2024-05-10 23:59:32 +0200

    dist updated

--- dist/jumbot/server/main.server.mjs
+++ dist/jumbot/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4CO7WI7V.mjs";import"./chunk-LGOCDVKN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2CI33R2N.mjs";import"./chunk-LGOCDVKN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
